6587483ddd1bc7a1236445bd251da0fee22fcbf7
RAHUL CHANDRAKANT CHAVAN <rahulcchavan.22@gmail.com>
2021-07-15 12:35:00 +0530

    initial

---
+++ TakeAtrip/src/main/java/com/project/TakeAtrip/TakeAtripApplication.java
@@ -0,0 +1,13 @@
+package com.project.TakeAtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeAtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeAtripApplication.class, args);
+	}
+
+}
---
+++ TakeAtrip/src/test/java/com/project/TakeAtrip/TakeAtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.TakeAtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeAtripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
